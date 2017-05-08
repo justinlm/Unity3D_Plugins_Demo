000cabf1e8752d4d37392d6451b141363b88a7ae
justinlm <justin.168@163.com>
2017-05-08 15:04:21 +0800

    ios update

---
+++ Assets/Scripts/Store/StoreProduct.cs
@@ -0,0 +1,43 @@
+﻿namespace IOSStore
+{
+	/// <summary>
+	/// Store product struct.
+	/// </summary>
+	public struct StoreProduct
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="id">Unique product identifier.</param>
+		/// <param name="title">Product title.</param>
+		/// <param name="price">ocalized product price (with price tag).</param>
+		/// <param name="description">Localized description.</param>
+		internal StoreProduct(string id, string title, string price, string description)
+		{
+		    ID = id;
+		    Title = title;
+		    Price = price;
+		    Description = description;
+		}
+
+		/// <summary>
+		/// Unique product identifier.
+		/// </summary>
+		public string ID { get; private set; }
+
+		/// <summary>
+		/// Product title.
+		/// </summary>
+		public string Title { get; private set; }
+
+		/// <summary>
+		/// Localized product price (with price tag).
+		/// </summary>
+		public string Price { get; private set; }
+
+		/// <summary>
+		/// Localized description.
+		/// </summary>
+		public string Description { get; private set; }
+	}
+}
